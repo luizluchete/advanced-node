75e8787cc0af5870393bc6280fe2a7123f79f928
Luiz Luchete <luizfernandolv@gmail.com>
2021-09-12 22:01:47 -0300

    move businnes logc to a domain entity

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,5 +1,6 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
+import { FacebookAccount } from '@/domain/models'
 import { LoadFacebookUserApi } from '../contracts/apis'
 import { SaveFacebookAccountRepository, LoadUserAccountRepository } from '../contracts/repos'
 
@@ -13,12 +14,9 @@ export class FacebookAuthenticationService {
     const fbData = await this.facebookApi.loadUser({ token: params.token })
     if (fbData !== undefined) {
       const accountData = await this.userAccountRepo.load({ email: fbData.email })
-      await this.userAccountRepo.saveWithFacebook({
-        id: accountData?.id,
-        name: accountData?.name ?? fbData.name,
-        email: fbData.email,
-        facebookId: fbData.facebookId
-      })
+      const fbAccount = new FacebookAccount(fbData, accountData)
+
+      await this.userAccountRepo.saveWithFacebook(fbAccount)
     }
     return new AuthenticationError()
   }
