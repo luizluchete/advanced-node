12ca17373ef46f9cad4109b87c76538f5b001d90
Luiz Luchete <luizfernandolv@gmail.com>
2021-09-13 21:42:45 -0300

    ensure ValidatorComposite returns the first error found

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,27 +1,44 @@
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 interface Validator {
   validate: () => Error | undefined
 }
 
-class ValidationComposite {
+class ValidationComposite implements Validator {
   constructor (private readonly validators: Validator[]) {}
-  validate (): undefined {
-    return undefined
+  validate (): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+      if (error !== undefined) {
+        return error
+      }
+    }
   }
 }
 
 describe('ValidationComposite', () => {
-  it('Should return undefied if all validators return undefied', () => {
-    const validator1 = mock<Validator>()
-    validator1.validate.mockReturnValue(undefined)
-    const validator2 = mock<Validator>()
-    validator2.validate.mockReturnValue(undefined)
-    const validators = [validator1, validator2]
-    const sut = new ValidationComposite(validators)
+  let sut: ValidationComposite
+  let validator1: MockProxy<Validator>
+  let validator2: MockProxy<Validator>
+  let validators: Validator[]
 
+  beforeAll(() => {
+    validator1 = mock()
+    validator1.validate.mockReturnValue(undefined)
+    validator2 = mock()
+    validator2.validate.mockReturnValue(undefined)
+    validators = [validator1, validator2]
+  })
+
+  beforeEach(() => {
+    sut = new ValidationComposite(validators)
+  })
+
+  it('Should return the first error', () => {
+    validator1.validate.mockReturnValue(new Error('error_1'))
+    validator2.validate.mockReturnValue(new Error('error_2'))
     const error = sut.validate()
 
-    expect(error).toBeUndefined()
+    expect(error).toEqual(new Error('error_1'))
   })
 })
