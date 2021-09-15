7bc18c86002c688ea54bdce398fa7da09f5ef553
Luiz Luchete <luizfernandolv@gmail.com>
2021-09-15 16:18:47 -0300

    fix ormconfig connection

--- src/application/errors/http.ts
+++ src/application/errors/http.ts
@@ -1,6 +1,5 @@
 export class ServerError extends Error {
   constructor (error?: Error) {
-    console.error(error?.message)
     super('Server failed. Try again soon')
     this.name = 'ServerError'
     this.stack = error?.stack
