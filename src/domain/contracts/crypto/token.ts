4692237bd9e271603e72780bf93e7328bf1081cc
Luiz Luchete <luizfernandolv@gmail.com>
2021-09-15 21:08:33 -0300

    move Authorize to its own file

--- src/domain/contracts/crypto/token.ts
+++ src/domain/contracts/crypto/token.ts
@@ -2,11 +2,20 @@ export interface TokenGenerator {
   generateToken: (params: TokenGenerator.Params) => Promise<TokenGenerator.Result>
 }
 
-export namespace TokenGenerator{
+export namespace TokenGenerator {
   export type Params = {
     key: string
     expirationInMs: number
   }
 
+  export type Result = string
+}
+
+export interface TokenValidator {
+  validateToken: (params: TokenValidator.Params) => Promise<TokenValidator.Result>
+}
+
+export namespace TokenValidator {
+  export type Params = { token: string }
   export type Result = string
 }
